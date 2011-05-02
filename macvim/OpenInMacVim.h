f2c89fce63b987307d71ca04d0e2f652aa32148d
Dan Michael Olsen Heggø <danmichaelo@gmail.com>
2011-05-02 22:57:50 +0200

    Added MacVim plugin

---
+++ macvim/OpenInMacVim.h
@@ -0,0 +1,28 @@
+//
+//  OpenInMacVim.h
+//  pathfinder_open_in_macvim
+//
+//  Created by orta therox on 06/09/2010.
+//  Copyright 2010 wgrids. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+  // if XCode warns that it can't find this file, you need to setup your 
+  // header_searcarch_paths to the Pathfinder SDK 
+  // HEADER_SEARCH_PATHS = "../../CocoatechPluginProtocols"
+  // by default it will look for the folder in your downloads
+
+#import "NTMenuPluginProtocol.h"
+
+
+@interface OpenInMacVim : NSObject <NTMenuPluginProtocol> {
+  id<NTPathFinderPluginHostProtocol> host;
+
+}
+- (NSMenuItem*)contextualMenuItem;
+- (NSMenuItem*)menuItem;
+
+@property (retain) id host;
+
+@end
---
+++ macvim/OpenInMacVim.m
@@ -0,0 +1,80 @@
+//
+//  OpenInMacVim.m
+//  pathfinder_open_in_macvim
+//
+//  Created by orta therox on 06/09/2010.
+//  Copyright 2010 wgrids. All rights reserved.
+//
+
+#import "OpenInMacVim.h"
+
+
+@implementation OpenInMacVim
+
+@synthesize host;
+
++ (id)plugin:(id<NTPathFinderPluginHostProtocol>)pathfinder_host;
+{
+  OpenInMacVim* result = [[self alloc] init];
+  result.host = pathfinder_host;
+  
+  return [result autorelease];
+}
+
+- (NSMenuItem*)contextualMenuItem;
+{
+	return [self menuItem];
+}
+
+- (NSMenuItem*)menuItem;
+{
+  NSMenuItem* menuItem;
+	
+  menuItem = [[[NSMenuItem alloc] initWithTitle:@"Open in MacVim" action:@selector(pluginAction:) keyEquivalent:@"m"] autorelease];
+  [menuItem setTarget:self];
+	[menuItem setKeyEquivalentModifierMask: NSControlKeyMask | NSCommandKeyMask];
+  return menuItem;
+}
+
+- (void)pluginAction:(id)sender;
+{
+  [self processItems:nil parameter:nil];
+}
+
+- (BOOL)validateMenuItem:(NSMenuItem*)menuItem;
+{
+  return [[[self host] selection:nil browserID:nil] count] == 1;
+}
+
+- (id)processItems:(NSArray*)items parameter:(id)parameter;
+{
+	if (!items)
+		items = [self.host  selection:nil browserID:nil];
+	
+    // Look for the files selected and turn it into one nice string
+  NSEnumerator* enumerator = [items objectEnumerator];
+  NSString *path;
+  id<NTFSItem> item;
+	NSMutableString* output = [NSMutableString string];
+	
+  while (item = [enumerator nextObject])
+   {
+		path = [item path];
+		
+		if (path)
+     {
+			[output appendString:path];
+     }
+   }
+  
+    //This is a round about way of doing it, and there's the limit of
+    // only allowing one file to be worked on at once but it's good enough for
+    // the minute, and for me.
+  
+  [[NSWorkspace sharedWorkspace] openFile: output withApplication:@"MacVim"];
+    
+  return nil;
+}
+
+
+@end
